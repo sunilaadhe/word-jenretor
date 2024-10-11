d3cae8ba7956305b59dcb8191110accb6fdc8eb0
sunilaadhe <sunilaadhe62@gmail.com>
2024-10-11 21:55:24 +0530

    just startet

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+let input = document.querySelector("input");
+
+let word = async ()=>{
+    let response = await fetch( `https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`);
+    let data = response.json();
+    console.log(data)
+}
+
+
+input.addEventListener("click",(e)=>{
+    word() 
+})
+
