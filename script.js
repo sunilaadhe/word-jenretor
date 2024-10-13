5413c38db1e840a7d36ed2233f14a625b5c490b3
sunilaadhe <sunilaadhe62@gmail.com>
2024-10-13 20:45:39 +0530

    litile

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 let input = document.querySelector("input");
 
 let word = async ()=>{
-    let response = await fetch( `https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`);
-    let data = response.json();
+    let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`);
+    let data =  await response.json();
     console.log(data)
 }
 
