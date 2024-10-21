e283f8c5d8af8db9a4a624f19f247bce14e1c16d
sunilaadhe <sunilaadhe62@gmail.com>
2024-10-21 20:02:10 +0530

    error

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 
-
-
 let input = document.querySelector("input");
-
 let word = async ()=>{
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`);
     let data =  await response.json();
     console.log(data)
 }
-
-
 input.addEventListener("click",(e)=>{
     word() 
 })
-
